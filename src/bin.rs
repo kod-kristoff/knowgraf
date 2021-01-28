29a698d1228f51fcfa7845115fab5f18e474d5a7
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-01-28 20:47:35 +0100

    feat: add post_store stub

--- src/bin.rs
+++ src/bin.rs
@@ -1,17 +1,18 @@
 use oxigraph::SledStore;
 use actix_files::NamedFile;
-use actix_web::{Responder, Result};
+use actix_web::{get, post, web, HttpRequest, Responder, Result};
 use std::path::PathBuf;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::{web, App, HttpServer};
-    println!("main");
+    use actix_web::{App, HttpServer};
+    println!("Starting server on 127.0.0.1:8080 ...");
     let store = SledStore::open("example.db");
 
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
+            .service(get_index)
+            .service(post_store)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -19,7 +20,13 @@ async fn main() -> std::io::Result<()> {
 
 }
 
-async fn index() -> Result<NamedFile> {
+#[get("/")]
+async fn get_index() -> Result<NamedFile> {
     let path = PathBuf::from("templates/index.html");
     Ok(NamedFile::open(path)?)
+}
+
+#[post("/store")]
+async fn post_store(req: HttpRequest) -> impl Responder {
+    format!("target: {}", req.query_string())
 }
