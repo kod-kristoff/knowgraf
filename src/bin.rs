caa8d80359d24c0ee530a9873e120ffc3fadee98
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-01-28 15:36:25 +0100

    feat: add actix server

--- src/bin.rs
+++ src/bin.rs
@@ -1,6 +1,25 @@
 use oxigraph::SledStore;
+use actix_files::NamedFile;
+use actix_web::{Responder, Result};
+use std::path::PathBuf;
 
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{web, App, HttpServer};
     println!("main");
     let store = SledStore::open("example.db");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+
+}
+
+async fn index() -> Result<NamedFile> {
+    let path = PathBuf::from("templates/index.html");
+    Ok(NamedFile::open(path)?)
 }
