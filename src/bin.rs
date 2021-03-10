d24a0e4763e8a0b00f873c1a3cc931b8c9bafb1e
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-03-10 12:52:11 +0100

    feat: stuff

--- src/bin.rs
+++ src/bin.rs
@@ -1,17 +1,20 @@
 use oxigraph::SledStore;
 use actix_files::NamedFile;
-use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder, Result};
+use actix_web::{get, post, web, HttpRequest, Responder, Result};
+use serde_derive::Deserialize;
 use std::path::PathBuf;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
+    env_logger::init();
     println!("Starting server on 127.0.0.1:8080 ...");
     let store = SledStore::open("example.db");
 
     HttpServer::new(|| {
         App::new()
             .service(get_index)
+            .service(get_query)
             .service(post_store)
     })
     .bind(("127.0.0.1", 8080))?
@@ -26,6 +29,12 @@ async fn get_index() -> Result<NamedFile> {
     Ok(NamedFile::open(path)?)
 }
 
+#[get("/query")]
+async fn get_query(form: web::Query<QueryData>) -> impl Responder {
+    log::info!("query: {:?}", form);
+    format!("get_query: {:?}", form)
+}
+
 #[post("/store")]
 async fn post_store(req: HttpRequest) -> impl Responder {
     if let Some(content_type) = req.headers().get("content-type") {
@@ -33,4 +42,11 @@ async fn post_store(req: HttpRequest) -> impl Responder {
     } else {
         HttpResponse::BadRequest().body("No Content-Type given.")
     }
+}
+
+#[derive(Deserialize, Debug)]
+struct QueryData {
+    query: String,
+    default_graph_uri: Vec<String>,
+    named_graph_uri: String,
 }
