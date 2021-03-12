4fd095e9257534b89c890f2ca9f971f9125df056
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-03-12 23:26:12 +0100

    refactor: move cli app to own module

---
+++ cli/src/cli.rs
@@ -0,0 +1,46 @@
+use clap::{App, Arg, SubCommand};
+
+pub fn build_cli() -> App<'static, 'static> {
+    use clap::{crate_version, crate_authors};
+    App::new("knowgraf-cli")
+        .version(crate_version!())
+        .author(crate_authors!(","))
+        .about("Command-line interface to knowgraf.")
+        .arg(Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("PATH")
+                .help("Specify the db")
+                .required(true))
+        .subcommand(SubCommand::with_name("query")
+            .about("query the graf")
+            .arg(Arg::with_name("query")
+                .short("q")
+                .long("query")
+                .value_name("INPUT")))
+        .arg(Arg::with_name("update")
+                .short("u")
+                .long("update")
+                .value_name("INPUT"))
+        .arg(Arg::with_name("store")
+                .short("s")
+                .long("store")
+                .value_name("INPUT"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn file_is_required() {
+        let args = vec!["kg-cli", ];
+        let m = build_cli().get_matches_from_safe(args);
+
+        match m {
+            Err(_) => {},
+            _ => panic!("should not be here."),
+        }
+    }
+
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,40 +1,19 @@
 use anyhow::Error;
 
+mod cli;
+
 fn main() {
-    match cli() {
+    match run_cli() {
         Ok(_) => {},
         Err(err) => eprintln!("Error: {}", err),
     }
 }
 
-fn cli() -> Result<(), Error> {
-    use clap::{crate_version, crate_authors};
+fn run_cli() -> Result<(), Error> {
     use clap::{Arg, App};
     use oxigraph::SledStore;
 
-    let matches = App::new("knowgraf-cli")
-        .version(crate_version!())
-        .author(crate_authors!(","))
-        .about("Command-line interface to knowgraf.")
-        .arg(Arg::with_name("file")
-                .short("f")
-                .long("file")
-                .value_name("PATH")
-                .help("Specify the db")
-                .required(true))
-        .arg(Arg::with_name("query")
-                .short("q")
-                .long("query")
-                .value_name("INPUT"))
-        .arg(Arg::with_name("update")
-                .short("u")
-                .long("update")
-                .value_name("INPUT"))
-        .arg(Arg::with_name("store")
-                .short("s")
-                .long("store")
-                .value_name("INPUT"))
-        .get_matches();
+    let matches = cli::build_cli().get_matches();
 
     let db_path = matches.value_of("file").unwrap();
     println!("Value of file: {:?}", db_path);
