c668a6782f3b270b21e1bc47fa5f20f537eac65f
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2021-03-12 09:21:10 +0100

    feat: add cli structure

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,31 @@
+use anyhow::Error;
+
 fn main() {
-    println!("Hello, world!");
+    match cli() {
+        Ok(_) => {},
+        Err(err) => eprintln!("Error: {}", err),
+    }
+}
+
+fn cli() -> Result<(), Error> {
+    use clap::{crate_version, Arg, App};
+    use oxigraph::SledStore;
+
+    let matches = App::new("knowgraf cli")
+        .version(crate_version!())
+        .author("Kristoffer Andersson <kod.kristoff@gmail.com>")
+        .about("Command-line interface to knowgraf.")
+        .arg(Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("PATH")
+                .help("Specify the db")
+                .required(true))
+        .get_matches();
+
+    let db_path = matches.value_of("file").unwrap();
+    println!("Value of file: {:?}", db_path);
+
+    let store = SledStore::open(db_path);
+    Ok(())
 }
