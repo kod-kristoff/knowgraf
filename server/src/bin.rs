31343b570ae1a8ab9de94bfeffa0298cc0bed1f3
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2021-03-16 11:00:09 +0100

    feat: work

--- server/src/bin.rs
+++ server/src/bin.rs
@@ -1,8 +1,9 @@
 use oxigraph::SledStore;
+use oxigraph::io::DatasetFormat;
+use oxigraph::io::GraphFormat;
 use actix_files::NamedFile;
 use actix_web::{
     get,
-    post,
     web,
     HttpRequest,
     HttpResponse,
@@ -12,21 +13,31 @@ use actix_web::{
 use serde_derive::Deserialize;
 use std::path::PathBuf;
 
+struct AppState {
+    store: SledStore,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
     env_logger::init();
     println!("Starting server on 127.0.0.1:8080 ...");
-    let store = SledStore::open("example.db");
+    let app_state = web::Data::new(AppState {
+        store: SledStore::open("example.db")?
+    });
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(app_state.clone())
             .service(
                 web::resource("/")
                     .route(web::get().to(get_index))
             )
             .service(get_query)
-            .service(post_store)
+            .service(
+                web::resource("/store")
+                    .route(web::post().to(post_store))
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -46,9 +57,12 @@ async fn get_query(form: web::Query<QueryData>) -> impl Responder {
     format!("get_query: {:?}", form)
 }
 
-#[post("/store")]
-async fn post_store(req: HttpRequest) -> impl Responder {
+// #[post("/store")]
+async fn post_store(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
     if let Some(content_type) = req.headers().get("content-type") {
+        println!("content-type: {:?}", content_type);
+        println!("graphformat: {:?}", GraphFormat::from_media_type(content_type.to_str().unwrap()));
+        println!("datasetformat: {:?}", DatasetFormat::from_media_type(content_type.to_str().unwrap()));
         HttpResponse::Ok().body(format!("target: {}\ncontent-type: {:?}", req.query_string(), content_type))
     } else {
         HttpResponse::BadRequest().body("No Content-Type given.")
@@ -79,23 +93,70 @@ mod tests {
         let req = test::TestRequest::with_header("content-type", "text/plain").to_request();
         let resp = test::call_service(&mut app, req).await;
         assert!(resp.status().is_success());
-    } 
+    }
 
-    #[actix_rt::test]
-    async fn post_dataset_file() {
-        let mut app = test::init_service(
-            App::new()
-                .service(
-                    web::resource("/")
-                        .route(web::get().to(get_index))
-                )
-        ).await;
-        let req = test::TestRequest::post()
-            .uri("/store")
-            .header("Content-Type", "application/trig")
-            .set_payload("<http://example.com> <http://example.com> <http://example.com> .")
-            .to_request();
-        let resp = test::call_service(&mut app, req).await;
-        assert_eq!(resp.status(), http::StatusCode::NO_CONTENT);
+    mod store {
+        use super::*;
+
+        #[actix_rt::test]
+        async fn post_dataset_file() {
+            let mut app = test::init_service(
+                App::new()
+                    .service(
+                        web::resource("/")
+                            .route(web::get().to(get_index))
+                    )
+                    .service(
+                        web::resource("/store")
+                            .route(web::post().to(post_store))
+                    )
+            ).await;
+            let req = test::TestRequest::post()
+                .uri("/store")
+                .header("Content-Type", "application/trig")
+                .set_payload("<http://example.com> <http://example.com> <http://example.com> .")
+                .to_request();
+            let resp = test::call_service(&mut app, req).await;
+            assert_eq!(resp.status(), http::StatusCode::NO_CONTENT);
+        }
+
+        #[actix_rt::test]
+        async fn post_no_content() {
+            let mut app = test::init_service(
+                App::new()
+                    .service(
+                        web::resource("/")
+                            .route(web::get().to(get_index))
+                    )
+                    .service(
+                        web::resource("/store")
+                            .route(web::post().to(post_store))
+                    )
+            ).await;
+            let req = test::TestRequest::post()
+                .uri("/store")
+                .set_payload("<http://example.com> <http://example.com> <http://example.com> .")
+                .to_request();
+            let resp = test::call_service(&mut app, req).await;
+            assert_eq!(resp.status(), http::StatusCode::BAD_REQUEST);
+        }
+
+        #[actix_rt::test]
+        async fn post_unsupported_file() {
+            let mut app = test::init_service(
+                App::new()
+                    .service(
+                        web::resource("/")
+                            .route(web::get().to(get_index))
+                    )
+                    .service(
+                        web::resource("/store")
+                            .route(web::post().to(post_store))
+                    )
+            ).await;
+            let req = test::TestRequest::with_header("content-type", "text/plain").uri("/store").to_request();
+            let resp = test::call_service(&mut app, req).await;
+            assert_eq!(resp.status(), http::StatusCode::UNSUPPORTED_MEDIA_TYPE);
+        }
     }
 }
